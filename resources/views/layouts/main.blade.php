82cbcc5fe7fe012937e3d238a5b6ae5006747ec4
notquarks <abhyasadanub@gmail.com>
2022-06-17 16:11:14 +0700

    edit top navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,101 +1,93 @@
 <!DOCTYPE html>
 <html lang="en">
 @yield('head')
-  <body class="nav-body">
+
+<body class="nav-body">
     <!-- nav bar -->
     <input type="checkbox" id="checkbox-toggle" onclick="openNav()" checked />
     <div class="hb-container">
-      <span class="hb"></span>
-      <span class="hb"></span>
-      <span class="hb"></span>
+        <span class="hb"></span>
+        <span class="hb"></span>
+        <span class="hb"></span>
     </div>
     <div class="container-sidenavbar" id="side-navbar">
-      <div class="disp-flex flex-row center-vertical">
-        <div class="title-container center-vertical">
-          <img src="./assets/logo/logo.png" alt="" /><span>SIPALING</span>
+        <div class="disp-flex flex-row center-vertical">
+            <div class="title-container center-vertical">
+                <img src="./assets/logo/logo.png" alt="" /><span>SIPALING</span>
+            </div>
         </div>
-      </div>
-      <div class="instansi-container">
-        <img src="./assets/logo/uns.png" alt="" srcset="" />
-        <span>Universitas Sebelas Maret</span>
-      </div>
-      <div class="nav-menu">
-        <span>General</span>
-        <a href="/" >
-          <i class="fa-solid fa-house-user fa-lg " style="margin-right: 0.4em">
-          </i>
-          Dashboard
-        </a><i class="fa-solid fa-house-user fa-lg collapse-icon" ></i>
-        @auth
-        <div class="manajemen-container">
-          <span>Manajemen</span>
-          <a href="{{ route('pengajuan_cuti') }}">
-            <i
-              class="fa-solid fa-calendar-days fa-lg move-icon"
-              style="margin-right: 0.4em; margin-left: 0.1em"
-            >
-            </i>
-            Pengajuan Cuti
-          </a><i
-          class="fa-solid fa-calendar-days fa-lg collapse-icon"></i>
-
-          @if(Auth::user()->is_admin == 1)
-          <a href="{{ route('admin.approvalCuti') }}">
-            <i
-              class="fa-solid fa-bell fa-lg move-icon"
-              style="margin-right: 0.4em; margin-left: 0.1em"
-            >
-            </i>
-            Approval Cuti
-          </a><i
-          class="fa-solid fa-bell fa-lg collapse-icon"></i>
-          @endif
-
-          <a href="">
-            <i
-              class="fa-solid fa-list fa-lg move-icon"
-              style="margin-right: 0.4em; margin-left: 0.1em"
-            >
-            </i>
-            Data History
-          </a><i class="fa-solid fa-list fa-lg collapse-icon"></i>
+        <div class="instansi-container">
+            <img src="./assets/logo/uns.png" alt="" srcset="" />
+            <span>Universitas Sebelas Maret</span>
         </div>
-        @endauth
-      </div>
+        <div class="nav-menu">
+            <span>General</span>
+            <a href="/">
+                <i class="fa-solid fa-house-user fa-lg " style="margin-right: 0.4em">
+                </i>
+                Dashboard
+            </a><i class="fa-solid fa-house-user fa-lg collapse-icon"></i>
+            @auth
+                <div class="manajemen-container">
+                    <span>Manajemen</span>
+                    <a href="{{ route('pengajuan_cuti') }}">
+                        <i class="fa-solid fa-calendar-days fa-lg move-icon"
+                            style="margin-right: 0.4em; margin-left: 0.1em">
+                        </i>
+                        Pengajuan Cuti
+                    </a><i class="fa-solid fa-calendar-days fa-lg collapse-icon"></i>
+
+                    @if (Auth::user()->is_admin == 1)
+                        <a href="{{ route('admin.approvalCuti') }}">
+                            <i class="fa-solid fa-bell fa-lg move-icon" style="margin-right: 0.4em; margin-left: 0.1em">
+                            </i>
+                            Approval Cuti
+                        </a><i class="fa-solid fa-bell fa-lg collapse-icon"></i>
+                    @endif
+
+                    <a href="">
+                        <i class="fa-solid fa-list fa-lg move-icon" style="margin-right: 0.4em; margin-left: 0.1em">
+                        </i>
+                        Data History
+                    </a><i class="fa-solid fa-list fa-lg collapse-icon"></i>
+                </div>
+            @endauth
+        </div>
     </div>
     <!-- main page -->
     <div class="disp-flex flex-col r-ctn-wdth">
         @auth
-        <!-- right corner user profile dropdown  -->
-      <div class="top-right-navbar" onclick="profileDropdwn()">
-        <img src="./assets/img/user-placeholder.png" alt="" srcset="" class="user-icon">
-          <span class="username">{{ explode(" ", ucwords(strtolower(Auth::user()->nama)))[0] }}</span>
-          <i
-            class="fa-solid fa-angle-down"              
-            style="position: relative; right: -8px; top: 1px; pointer-events: none;">
-          </i>
-        <ul class="prof-dropdown" id="profDropdown">
-          <li><a href="/profile">Profile</a></li>
-          <li>
-            <form action={{ route('logout') }} method="POST">
-              @csrf
-              <a href="#"><input type="submit" value="Logout" id="logout-btn" style="width: 225px; z-index:10;height: 50px; margin-top: 20px; display:hidden;">Logout</a>
-            </form>
-          </li>
-        </ul>
-      </div>
+            <!-- right corner user profile dropdown  -->
+            <div class="top-right-navbar" onclick="profileDropdwn()">
+                <img src="./assets/img/user-placeholder.png" alt="" srcset="" class="user-icon">
+                <span class="username">{{ explode(' ', ucwords(strtolower(Auth::user()->nama)))[0] }}</span>
+                <i class="fa-solid fa-angle-down" style="position: relative; right: -8px; top: 1px; pointer-events: none;">
+                </i>
+                <ul class="prof-dropdown" id="profDropdown">
+                    <li><a href="/profile">Profile</a></li>
+                    <li>
+                        <form action={{ route('logout') }} method="POST" id="logout-form">
+                            @csrf
+                            <a href="#" onclick="document.getElementById('logout-form').submit()">Logout</a>
+                            {{-- <input
+                                    type="submit" value="Logout" id="logout-btn"
+                                    style="width: 225px; z-index:10;height: 50px; margin-top: 20px; display:hidden;"> --}}
+                        </form>
+                    </li>
+                </ul>
+            </div>
         @else
+            <div class="nav-link">
+                <a href="{{ route('login') }}">Login</a>
+                <a href="{{ route('register') }}">Register</a>
+            </div>
 
-        <div class="nav-link">
-            <a href="{{ route('login') }}">Login</a>
-            <a href="{{ route('register') }}">Register</a>
-        </div>
-          
         @endauth
-      
-      <!-- main content -->
-      @yield('content')
-    
+
+        <!-- main content -->
+        @yield('content')
+
     </div>
-  </body>
+</body>
+
 </html>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,77 +1,79 @@
 @extends('layouts.main')
 
 @section('head')
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="assets/css/dashboardprofile.css" type="text/css"  />
-    <link rel="stylesheet" href="assets/css/fonts.css" type="text/css"  />
-    <link rel="stylesheet" href="assets/css/profile.css" type="text/css" />
-    <script src="assets/js/dashboard.js"></script>
-    <script src="https://kit.fontawesome.com/6ba9b8f714.js" crossorigin="anonymous"></script>
-    <title>SIPALING | Profile</title>
-  </head>
+
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="assets/css/dashboardprofile.css" type="text/css" />
+        <link rel="stylesheet" href="assets/css/fonts.css" type="text/css" />
+        <link rel="stylesheet" href="assets/css/profile.css" type="text/css" />
+        <script src="assets/js/dashboard.js"></script>
+        <script src="https://kit.fontawesome.com/6ba9b8f714.js" crossorigin="anonymous"></script>
+        <title>SIPALING | Profile</title>
+    </head>
 @endsection
 
 @section('content')
-<div class="global-profile-container">
-    <div style="width: 65%; margin: 0 10rem 1rem 20%; text-align:right;">
-      <button type="button" class="profile-edit-option" id="edit-button" onclick="openPopupPhoto()">
-        <i class="fa-regular fa-pen-to-square"></i>
-        Edit Photo Profile
-      </button>
+    <div class="global-profile-container">
+        <div style="width: 65%; margin: 0 10rem 1rem 20%; text-align:right;">
+            <button type="button" class="profile-edit-option" id="edit-button" onclick="openPopupPhoto()">
+                <i class="fa-regular fa-pen-to-square"></i>
+                Edit Photo Profile
+            </button>
+        </div>
+        <div class="pop-up-container-photo" id="photo">
+            <div class="drop-zone">
+                <span class="drop-zone__prompt">Drop file here or click to upload</span>
+                <input type="file" name="myFile" class="drop-zone__input" />
+            </div>
+            <button type="button" id="yes-button" onclick="closePopupPhoto()"><i
+                    class="fa-solid fa-check"></i>Save</button>
+            <button type="button" id="cancel-button" onclick="closePopupPhoto()"><i
+                    class="fa-solid fa-xmark"></i>Cancel</button>
+        </div>
+        <div class="profile-container">
+            <div class="profile-title">
+                <h1>Profile</h1>
+            </div>
+            <div class="profile-img">
+                <img src="assets/img/user-placeholder.png" alt="user" />
+                <h2>{{ auth()->user()->nama }}</h2>
+            </div>
+            <div class="profile-name">
+                <h3>NIK</h3>
+                <div id="user-name">{{ auth()->user()->nik }}</div>
+            </div>
+            <div class="profile-nip">
+                <h3>NIP</h3>
+                <div id="user-nip">{{ auth()->user()->nip }}</div>
+            </div>
+            <div class="profile-name">
+                <h3>Nama</h3>
+                <div id="user-name">{{ auth()->user()->nama }}</div>
+            </div>
+            <div class="profile-name">
+                <h3>Alamat</h3>
+                <div id="user-name">{{ auth()->user()->alamat }}</div>
+            </div>
+            <div class="profile-name">
+                <h3>Jenis kelamin</h3>
+                <div id="user-name">{{ auth()->user()->jenis_kelamin }}</div>
+            </div>
+            <div class="profile-name">
+                <h3>Jurusan</h3>
+                <div id="user-name">{{ $nama_department }}</div>
+            </div>
+            <div class="profile-name">
+                <h3>Email</h3>
+                <div id="user-name">{{ auth()->user()->email }}</div>
+            </div>
+        </div>
     </div>
-    <div class="pop-up-container-photo" id="photo">
-      <div class="drop-zone">
-        <span class="drop-zone__prompt">Drop file here or click to upload</span>
-        <input type="file" name="myFile" class="drop-zone__input" />
-      </div>
-      <button type="button" id="yes-button" onclick="closePopupPhoto()"><i class="fa-solid fa-check"></i>Save</button>
-      <button type="button" id="cancel-button" onclick="closePopupPhoto()"><i class="fa-solid fa-xmark"></i>Cancel</button>
     </div>
-    <div class="profile-container">
-      <div class="profile-title">
-        <h1>Profile</h1>
-      </div>
-      <div class="profile-img">
-        <img src="assets/img/user-placeholder.png" alt="user" />
-        <h2>{{ auth()->user()->nama }}</h2>
-      </div>
-      <div class="profile-name">
-        <h3>NIK</h3>
-        <div id="user-name">{{ auth()->user()->nik }}</div>
-      </div>
-      <div class="profile-nip">
-        <h3>NIP</h3>
-        <div id="user-nip">{{ auth()->user()->nip }}</div>
-      </div>
-      <div class="profile-name">
-        <h3>Nama</h3>
-        <div id="user-name">{{ auth()->user()->nama }}</div>
-      </div>
-      <div class="profile-name">
-        <h3>Alamat</h3>
-        <div id="user-name">{{ auth()->user()->alamat }}</div>
-      </div>
-      <div class="profile-name">
-        <h3>Jenis kelamin</h3>
-        <div id="user-name">{{ auth()->user()->jenis_kelamin }}</div>
-      </div>
-      <div class="profile-name">
-        <h3>Jurusan</h3>
-        <div id="user-name">{{$nama_department}}</div>
-      </div>
-      <div class="profile-name">
-        <h3>Email</h3>
-        <div id="user-name">{{ auth()->user()->email }}</div>
-      </div>
-    </div>
-  </div>
-</div>
 
     <script src="assets/js/profile.js"></script>
     <script src="assets/js/dragPhoto.js"></script>
     <script src="assets/js/popupPhoto.js"></script>
 @endsection
-
