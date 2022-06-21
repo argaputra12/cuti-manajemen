3ee0e49208f79c4686274b8005bb2edf20351a5b
ALDO ATTALLAH DUTA HARDEANDRA <84911024+HumanRight19@users.noreply.github.com>
2022-06-21 11:17:08 +0700

    Mengubah tampilan verifikasi email

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,28 +1,41 @@
 @extends('layouts.app')
 
+<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+<style>
+    h2{
+        font-family: 'Poppins', sans-serif;
+        font-size: 20px;
+        text-align: center;
+        padding-top: 10px; 
+    }
+
+    .btn{
+        max-width: fit-content;
+    }
+</style>
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
-                </div>
+<div class="wrapper p-20 h-[100vh]">
+    <div class="m-auto w-[400px] border-solid rounded-xl bg-white border-2 font-poppins drop-shadow-2xl">
+        <div class="text-center text-xl rounded-t-xl mb-2 font-semibold bg-[#11386d] text-white leading-[75px]">Verifikasi Email Anda</div>
+        
+        @if (session('resent'))
+            <div class="alert alert-success" role="alert">
+                {{ __('A fresh verification link has been sent to your email address.') }}
             </div>
-        </div>
+        @endif
+
+        <h2>
+            Sebelum melanjutkan, harap periksa email Anda untuk verifikasi lebih lanjut. Jika Anda tidak menerima email
+        </h2>
+
+        <form action="{{ route('verification.resend') }}" method="post" class="d-flex justify-content-center mb-4">
+            @csrf
+            <button type="submit" class="btn btn-link p-0 m-0 align-baseline">
+                <h2>klik di sini untuk kirim ulang email</h2>
+            </button>.
+        </form>
     </div>
 </div>
 @endsection
+
