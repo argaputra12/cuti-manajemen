3ea2e450026def7ec2071b6a23823b8f20ef2ea8
ALDO ATTALLAH DUTA HARDEANDRA <84911024+HumanRight19@users.noreply.github.com>
2022-06-21 11:26:36 +0700

    Menambah function construct

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Auth;
 class DashboardController extends Controller
 {
     //
+    public function __construct(){
+        $this->middleware(['auth','verified']);
+    }
+    
     public function index(){
 
         if(Auth::user()){
