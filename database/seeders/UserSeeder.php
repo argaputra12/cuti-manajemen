2ae6f2ebf3aefd916d2dfd018eb44d8fb3f0e10e
argaputra12 <argaputra@student.uns.ac.id>
2022-06-28 16:03:09 +0700

    add faker

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace Database\Seeders;
+// when installed via composer
+require_once 'vendor/autoload.php';
+
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -18,8 +23,8 @@ class UserSeeder extends Seeder
         //
         $inserted_data = [
             [
-                'nik' => '123456789',
-                'nip' => '123456789',
+                'nik' => '12345678934235',
+                'nip' => '1234',
                 'nama' => 'Admin',
                 'alamat' => 'Jl. Raya',
                 'jenis_kelamin' => 'laki-laki',
@@ -30,8 +35,8 @@ class UserSeeder extends Seeder
                 'sisa_cuti' => 99,
             ],
             [
-                'nik' => '1234567890',
-                'nip' => '1234567890',
+                'nik' => '12345678907542',
+                'nip' => '7890',
                 'nama' => 'User',
                 'alamat' => 'Jl. Raya',
                 'jenis_kelamin' => 'laki-laki',
@@ -45,6 +50,32 @@ class UserSeeder extends Seeder
 
         foreach ($inserted_data as $data) {
             User::create($data);
+        }
+
+        // Jumalh cuti maksimal tahun ini
+        $jumlah_cuti_maksimum = DB::table('konfigurasi_cutis')->where('tahun', now()->format('Y'))->first()->jumlah_cuti_maksimum;
+
+        // Jumlah cuti beersama tahun ini
+        $jumlah_cuti_bersama = DB::table('konfigurasi_cutis')->where('tahun', now()->format('Y'))->first()->jumlah_cuti_bersama;
+
+        $faker = Faker::create('id_ID');
+
+        for($i = 1; $i <= 50; $i++){
+
+            // insert data user dengan faker
+            DB::table('users')
+            ->insert([
+                'nip' => $faker->unique()->numberBetween(1000, 9999),
+                'nik' => $faker->unique()->numberBetween(100000000000000, 9999999999999999),
+                'nama' => $faker->name(),
+                'alamat' => $faker->address(),
+                'jenis_kelamin' => $faker->randomElement(['laki-laki', 'perempuan']),
+                'department_id' => $faker->numberBetween(1, 6),
+                'email' => $faker->unique()->email(),
+                'password' => bcrypt('password'),
+                'is_admin' => $faker->numberBetween(0, 1),
+                'sisa_cuti' => $jumlah_cuti_maksimum-$jumlah_cuti_bersama,
+            ]);
         }
     }
 }
